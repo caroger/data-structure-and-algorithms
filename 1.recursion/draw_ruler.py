dede06220b72d49db3d09e1d844e619e8f1f6260
caroger <handeasy@gmail.com>
2020-08-15 22:23:16 -0700

    draw-ruler recursion

---
+++ 1.recursion/draw_ruler.py
@@ -0,0 +1,14 @@
+def draw_line(tick_length, tick_label=""):
+    """Draw one line with given tick length"""
+    pass
+
+
+def draw_interval(center_length):
+    """Draw tick interval based upon a central tick length"""
+    pass
+
+
+def draw_ruler(num_inches, major_length):
+    """Draw English ruler with given number of inches, major tick length"""
+    pass
+
