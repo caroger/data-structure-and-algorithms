e106095af66b22daaa19b14227d30ef80329e3be
caroger <handeasy@gmail.com>
2020-09-12 15:45:51 -0700

    Ch1

---
+++ 3.recursion/binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(data, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search(data, target, low, mid - 1)
+        else:
+            return binary_search(data, target, mid + 1, high)
+
+
+if __name__ == "__main__":
+    my_data = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
+    print(binary_search(my_data, 1, 0, len(my_data)))
+    print(binary_search(my_data, 37, 0, len(my_data)))
